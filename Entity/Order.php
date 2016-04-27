c25ab053112a5135e053d59eba0ac8ac280b0922
ForeverGlory <foreverglory@qq.com>
2016-04-27 17:35:12 +0800

    entity

---
+++ Entity/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\PayBundle\Entity;
+
+use Glory\Bundle\PayBundle\Model\Order as BaseOrder;
+
+/**
+ * Description of Order
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class Order extends BaseOrder
+{
+    
+}
