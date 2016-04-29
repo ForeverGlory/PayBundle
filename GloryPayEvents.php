817aa04988edd6d035366dbe8bff968892958ba9
ForeverGlory <foreverglory@qq.com>
2016-04-29 08:40:48 +0800

    支付事件

---
+++ GloryPayEvents.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ */
+
+namespace Glory\Bundle\PayBundle;
+
+/**
+ * Description of GloryPayEvents
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+final class GloryPayEvents
+{
+    
+}
