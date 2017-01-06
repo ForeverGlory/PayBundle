312fa226efa382193bfb5b5ea2ac3dacf69938f6
ForeverGlory <foreverglory@qq.com>
2017-01-06 12:25:55 +0800

    支持直接使用流程

---
+++ Processer/PayProcesser.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ *
+ * (c) ForeverGlory <http://foreverglory.me/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\PayBundle\Processer;
+
+use Glory\Bundle\PayBundle\Model\PayInterface;
+
+class PayProcesser
+{
+
+    public function create(){
+
+    }
+
+    public function process(PayInterface $pay){
+
+    }
+
+    public function notify(){
+
+    }
+
+    public function comple(){
+
+    }
+
+}
